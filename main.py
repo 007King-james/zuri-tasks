bbc0557c7d51844fa41a231261ba3581fa691918
007King-james <105353413+007King-james@users.noreply.github.com>
2022-05-28 20:54:40 +0100

    Add files via upload
    Reading text file task

--- main.py
+++ main.py
@@ -1,20 +1,34 @@
-# Check if two words are anagrams
+# Read text from a file, and count the occurence of words in that text
 # Example:
-# find_anagrams('hello', 'check') --> False
-# find_anagrams('below', 'elbow') --> True
+# count_words("The cake is done. It is a big cake!") 
+# --> {"cake":2, "big":1, "is":2, "the":1, "a":1, "it":1}
 
-
-def find_anagram(word, anagram):
+def read_file_content(filename):
     # [assignment] Add your code here
+    with open(filename) as f:
+        lines = f.readlines()
     
-    if(sorted(word) == sorted(anagram)):
-        print("True")
-    else:
-        print("False")
+    return (lines)
 
-word = "below"
-anagram = "elbow"
-find_anagram(word, anagram)
 
-    
-        
+def count_words(file):
+    text = read_file_content("story.txt")
+    # [assignment] Add your code here
+    words = []
+    text = read_file_content(file)
+
+    for t in text:
+        t = t.split()
+
+        for i in t:
+            words.append(i)
+    print(words)
+    final_dict = {}
+    for word in words:
+        final_dict[word] = words.count(word)
+
+    print(final_dict)
+
+count_words("story.txt")
+
+
